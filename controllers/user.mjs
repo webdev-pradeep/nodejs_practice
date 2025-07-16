029a43519b4605c07233e863fc07876c71742f20
Pradeep <webdevpradeep0101@gmail.com>
2025-07-16 09:43:58 +0530

    feat: integrate Prisma for user registration and login functionality

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -1,6 +1,7 @@
 import { readFile, writeFile } from "node:fs/promises";
 
 import jwt from "jsonwebtoken";
+import prisma from "../db.mjs";
 
 const registerController = async (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password) {
@@ -9,30 +10,15 @@ const registerController = async (req, res, next) => {
     // throw new Error(JSON.stringify({ error: "input is not valid" }))
   }
 
-  const fileDataStr = await readFile("./db.json", {
-    encoding: "utf-8",
+  // add  user in db
+
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    },
   });
-  const fileData = JSON.parse(fileDataStr);
-  const userData = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  };
-
-  // check if user already registered
-  if (
-    fileData.users.filter((e) => {
-      return e.email === userData.email;
-    }).length > 0
-  ) {
-    res.statusCode = 400;
-    return res.json({ error: "user already registered" });
-  }
-  // user data added to json data
-  fileData.users.push(userData);
-
-  // db json update
-  await writeFile("./db.json", JSON.stringify(fileData));
   // send response
   res.json({ message: "register successful" });
 };
@@ -44,18 +30,15 @@ const loginController = async (req, res, next) => {
     return res.json({ error: "input is not valid" });
   }
 
-  // read db in string
-  const fileDataStr = await readFile("./db.json", { encoding: "utf-8" });
-  // convert db data in json
-  const dbData = JSON.parse(fileDataStr);
-
   // find user in db
-  const user = dbData.users.filter((e) => {
-    return e.email === req.body.email;
-  })[0];
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+  });
 
   // match password
-  if (user.password !== req.body.password) {
+  if (!user) {
     res.statusCode = 400;
     return res.json({ error: "password is wrong" });
   }
