42a69b80a0626af1a3ce66a42ca71bc8215031c2
Pradeep <webdevpradeep0101@gmail.com>
2025-07-19 11:09:57 +0530

    feat: implement forgot password functionality with token generation and email notification

--- email.mjs
+++ email.mjs
@@ -1,27 +1,17 @@
 import nodemailer from "nodemailer";
+import "dotenv/config";
 
-// // Create a test account or replace with real credentials.
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.ethereal.email",
-//   port: 587,
-//   auth: {
-//     user: "maddison53@ethereal.email",
-//     pass: "jn7jnAPss4f63QBp6D",
-//   },
-// });
-
-// Looking to send emails in production? Check out our Email API/SMTP product!
 var transport = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
+  host: process.env.EMAIL_HOST,
   port: 2525,
   auth: {
-    user: "887c25528eb947",
-    pass: "97e8a349acac94",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
 const sendEmail = async (to, subject, body) => {
-  const info = await transport.sendMail({
+  await transport.sendMail({
     from: "apple Server <a@apple.com>",
     to,
     subject,
---
+++ prisma/migrations/20250719045727_token/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "resetToken" TEXT DEFAULT '',
+ADD COLUMN     "resetTokenExpiry" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
