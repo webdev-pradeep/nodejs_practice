b3e4d9aa81f9b2593e58be21ac499490f226fcc0
Pradeep <webdevpradeep0101@gmail.com>
2025-07-22 07:46:12 +0530

    feat: add reset password functionality with token validation and password update

--- email.mjs
+++ email.mjs
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 var transport = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 2525,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
