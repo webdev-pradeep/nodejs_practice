4f5909d192ad66a7ca9137ef00069e06b3cda62e
Pradeep <webdevpradeep0101@gmail.com>
2025-07-23 07:45:05 +0530

    feat: enhance error handling by adding manual error response and exporting ServerError class

--- error.mjs
+++ error.mjs
@@ -1,10 +1,25 @@
 const errorController = (err, req, res, next) => {
   console.log(err);
-  res.json({ error: "something is not ok" });
+
+  if (err.isManual) {
+    res.statusCode = res.statusCode;
+    res.json({ error: err.message });
+  } else {
+    res.statusCode = 500;
+    res.json({ error: "something is not ok" });
+  }
 };
 
 const undefinedRouteHandler = (req, res) => {
   res.json({ message: "wrong route" });
 };
 
-export { errorController, undefinedRouteHandler };
+class ServerError extends Error {
+  constructor(statusCode, errorMassage) {
+    super(errorMassage);
+    this.statusCode = statusCode;
+    this.isManual = true;
+  }
+}
+
+export { errorController, undefinedRouteHandler, ServerError };
